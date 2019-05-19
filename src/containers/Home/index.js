57d6b082b896d72448f57747fffa23f9c9a566fc
Edward-1020 <iezzuyoung@gmail.com>
2019-05-19 16:58:54 +0800

    feat: Head

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header';
 
 const Home = () => {
     return (
         <div>
+            <Header />
             <div>This is Dell Lee!</div>
             <button onClick={() => {alert('click')}}>
                 click
