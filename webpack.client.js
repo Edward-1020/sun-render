af7b8bfd4c7a5e6edd32860b2dcb127422f23caf
Edward-1020 <iezzuyoung@gmail.com>
2019-05-19 14:25:39 +0800

    feat: 目录结构

--- webpack.client.js
+++ webpack.client.js
@@ -1,19 +1,15 @@
 const path = require('path');
+const merge = require('webpack-merge');
 
-module.exports = {
+const baseConfig = require('./webpack.base.js');
+
+const clientConfig = {
     mode: 'development',
     entry: './src/client/index.js',
     output: {
         filename: 'index.js',
         path: path.resolve(__dirname, 'public')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
     }
-}
+};
+
+module.exports = merge(baseConfig, clientConfig);
--- webpack.server.js
+++ webpack.server.js
@@ -1,22 +1,18 @@
 const path = require('path');
+const merge = require('webpack-merge');
+
+const baseConfig = require('./webpack.base.js');
 const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
+const serverConfig = {
     target: 'node',
     mode: 'development',
-    entry: './src/index.js',
+    entry: './src/server/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
     },
-    externals: [nodeExternals()],
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/
-            }
-        ]
-    }
-}
+    externals: [nodeExternals()]
+};
+
+module.exports = merge(baseConfig, serverConfig);
